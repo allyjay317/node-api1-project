23cb43e905258687dfa3d693bfb0b7991e45fd44
Allison Usher <allyjay317@gmail.com>
2020-09-02 18:54:59 -0700

    added blocked out endpoints

--- index.js
+++ index.js
@@ -4,10 +4,30 @@ const server = express()
 
 const PORT = 5000;
 
+server.use(express.json())
+
 server.get('/', (req, res) => {
   res.status(200).send('Hello World')
 })
 
+server.post('/api/users', (req, res) => {
 
+})
+
+server.get('api/users', (req, res) => {
+
+})
+
+server.get('/api/users/:id', (req, res) => {
+
+})
+
+server.delete('/api/users/:id', (req, res) => {
+
+})
+
+server.put('/api/users/:id', (req, res) => {
+
+})
 
 server.listen(PORT)
